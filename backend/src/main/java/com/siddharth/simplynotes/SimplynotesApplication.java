9c2a816eb1b2e22a96b933979a98b29cff11afd3
musxsid <siddharthshakya2004@gmail.com>
2026-04-28 23:28:04 +0530

    configure PostgreSQL datasource

---
+++ backend/src/main/java/com/siddharth/simplynotes/SimplynotesApplication.java
@@ -0,0 +1,13 @@
+package com.siddharth.simplynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplynotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplynotesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/siddharth/simplynotes/SimplynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddharth.simplynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplynotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
